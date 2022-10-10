818e084b69ce8fa68f822e2b53793425b5ea9a56
jawahargit <87929156+jawahargit@users.noreply.github.com>
2022-10-10 15:57:48 +0530

    change to html.parser

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from bs4 import BeautifulSoup
 import requests
 url = requests.get('https://realpython.com/atom.xml')
 
-soup = BeautifulSoup(url.content, 'xml')
+#soup = BeautifulSoup(url.content, 'xml')
+#if we use XML parser - error is comins as ->  bs4.FeatureNotFound: Couldn't find a tree builder with the features you requested: xml. Do you need to install a 
+#parser library?
+# so change to html.parser - its working
+
+soup = BeautifulSoup(url.content, "html.parser")
 entries = soup.find_all('entry')
 rss = []
 for i in entries:
